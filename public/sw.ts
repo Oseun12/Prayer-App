9697bfee36045da3c92301af756e4da785b628a2
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2025-04-13 14:07:25 +0100

    Chore: vercel error checking

--- public/sw.ts
+++ public/sw.ts
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'prayer-app-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/main.js',
-  '/audio/background-music.mp3',
-  '/images/icon-192x192.png',
-  '/images/icon-512x512.png'
-];
+// const CACHE_NAME = 'prayer-app-v1';
+// const urlsToCache = [
+//   '/',
+//   '/index.html',
+//   '/styles.css',
+//   '/main.js',
+//   '/audio/background-music.mp3',
+//   '/images/icon-192x192.png',
+//   '/images/icon-512x512.png'
+// ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
+// self.addEventListener('install', (event: ExtendableEvent) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then((cache) => cache.addAll(urlsToCache))
+//   );
+// });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
+// self.addEventListener('fetch', (event: FetchEvent) => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => response || fetch(event.request))
+//   );
+// });
